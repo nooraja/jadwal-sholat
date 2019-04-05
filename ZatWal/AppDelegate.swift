21e17daaedff0c04d92a57c847de23b6f8797566
nooraja <noor@strongbee.co.id>
2019-04-05 22:20:13 +0700

    Trying using Core Location to get address

--- ZatWal/AppDelegate.swift
+++ ZatWal/AppDelegate.swift
@@ -7,43 +7,105 @@
 //
 
 import UIKit
+import CoreLocation
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    static let geoCoder = CLGeocoder()
+    let center = UNUserNotificationCenter.current()
+    let locationManager = CLLocationManager()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
         window?.makeKeyAndVisible()
         window?.rootViewController = SplashScreenViewController()
         
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+        }
+        locationManager.requestAlwaysAuthorization()
+        
+        locationManager.startMonitoringVisits()
+        locationManager.delegate = self
+        
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+}
+
+extension AppDelegate: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
+        // create CLLocation from the coordinates of CLVisit
+        let clLocation = CLLocation(latitude: visit.coordinate.latitude, longitude: visit.coordinate.longitude)
+        
+        // Get location description
+        AppDelegate.geoCoder.reverseGeocodeLocation(clLocation) { placemarks, _ in
+            if let place = placemarks?.first {
+                let description = "\(place)"
+                self.newVisitReceived(visit, description: description)
+            }
+        }
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    func newVisitReceived(_ visit: CLVisit, description: String) {
+        let location = Location(visit: visit, descriptionString: description)
+        LocationsStorage.shared.saveLocationOnDisk(location)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "New Journal entry 📌"
+        content.body = location.description
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest(identifier: location.dateString, content: content, trigger: trigger)
+        
+        center.add(request, withCompletionHandler: nil)
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
+        
+        AppDelegate.geoCoder.reverseGeocodeLocation(location) { placemarks, _ in
+            if let place = placemarks?.first {
+                let description = "Fake visit: \(place)"
+                
+                let fakeVisit = FakeVisit(coordinates: location.coordinate, arrivalDate: Date(), departureDate: Date())
+                self.newVisitReceived(fakeVisit, description: description)
+            }
+        }
     }
+}
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+final class FakeVisit: CLVisit {
+    private let myCoordinates: CLLocationCoordinate2D
+    private let myArrivalDate: Date
+    private let myDepartureDate: Date
+    
+    override var coordinate: CLLocationCoordinate2D {
+        return myCoordinates
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    override var arrivalDate: Date {
+        return myArrivalDate
     }
-
-
+    
+    override var departureDate: Date {
+        return myDepartureDate
+    }
+    
+    init(coordinates: CLLocationCoordinate2D, arrivalDate: Date, departureDate: Date) {
+        myCoordinates = coordinates
+        myArrivalDate = arrivalDate
+        myDepartureDate = departureDate
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
